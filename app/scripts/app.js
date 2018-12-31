40effd7ccf90d64dc833a22a6c7c4408858d5f91
Maya Soloveva <maya042@list.ru>
2018-12-31 15:45:11 +0300

    Доверстала анкету + pixel perfect

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,10 @@
 import svg4everybody from 'svg4everybody';
 import $ from 'jquery';
+require('./range.js');
+
+console.log('app.js');
 
 $(() => {
 	svg4everybody();
 });
+
---
+++ app/scripts/range.js
@@ -0,0 +1,57 @@
+console.log('range.js');
+
+function makeRange(rangeId) {
+	const slider = document.querySelector(rangeId);
+	const button = document.querySelector(rangeId + ' .range__button');
+	console.log('rangeId: ' + rangeId);
+	console.log('button: ' + rangeId + ' .range__button');
+
+	button.onmousedown = function (evt) {
+
+		function getCoords(elem) {
+			const box = elem.getBoundingClientRect();
+
+			return {
+				top: box.top + pageYOffset,
+				left: box.left + pageXOffset
+			};
+		}
+
+		const buttonCoords = getCoords(button);
+		const shiftX = evt.pageX - buttonCoords.left;
+		const sliderCoords = getCoords(slider);
+		console.log('Drag started');
+
+
+		document.onmousemove = function (event) {
+			let left = event.pageX - shiftX - sliderCoords.left;
+			let right = slider.offsetWidth - button.offsetWidth;
+
+			if (left < 0) {
+				left = 0;
+			}
+
+			if (left > right) {
+				left = right;
+			}
+
+			button.style.left = left + 'px';
+		};
+
+		document.onmouseup = function () {
+			document.onmousemove = document.onmouseup = null;
+			console.log('Drag eneded');
+		};
+
+		return false;
+	};
+
+	// Disable hthml5 drag and drop
+	button.ondragstart = function () {
+		return false;
+	};
+
+}
+
+
+makeRange('#range-level-js');
